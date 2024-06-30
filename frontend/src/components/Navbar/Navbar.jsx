de79eac789da6e433bb54634380ad35da59005a8
kariDV <karina.serrano@davinci.edu.ar>
2024-06-30 00:35:26 -0300

    se realizan arreglos

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import favoritoSvg from "../../assets/img/favorito.svg";
+// import favoritoSvg from "../../assets/img/favorito.svg";
 import logoSvg from "../../assets/img/logo.svg";
 
 const Navbar = () => {
@@ -45,11 +45,16 @@ const Navbar = () => {
                       className="btn link-nav-user text-center"
                     >
                       <span>
-                        <img
-                          src={favoritoSvg}
-                          class="ms-1 logo-t"
-                          alt="favorito"
-                        />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="23"
+                          height="23"
+                          fill="currentColor"
+                          className="bi bi-suit-heart-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
+                        </svg>
                       </span>
                       Mis favoritos
                     </NavLink>
