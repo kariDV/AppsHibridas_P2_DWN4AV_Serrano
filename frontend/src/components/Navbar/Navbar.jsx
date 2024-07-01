159b378ebbbf9dd7d85d4a780a0600a483884f87
kariDV <karina.serrano@davinci.edu.ar>
2024-07-01 20:05:37 -0300

    se modifica ver nav user logueado

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -31,49 +31,53 @@ const Navbar = () => {
 
             <div>
               <div className='m-2 d-flex'>
-                <ul className='navbar-nav'>
-                  <li className='nav-item'>
-                    <NavLink to='/Subir' className='btn link-nav-user'>
-                      <span>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          width='20'
-                          height='20'
-                          fill='currentColor'
-                          className='bi bi-bookmark-plus-fill'
-                          viewBox='0 0 16 16'
-                        >
-                          <path
-                            fillRule='evenodd'
-                            d='M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5z'
-                          />
-                        </svg>
-                      </span>
-                      Subir
-                    </NavLink>
-                  </li>
+                {user ? (
+                  <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                      <NavLink to='/Subir' className='btn link-nav-user'>
+                        <span>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='20'
+                            height='20'
+                            fill='currentColor'
+                            className='bi bi-bookmark-plus-fill'
+                            viewBox='0 0 16 16'
+                          >
+                            <path
+                              fillRule='evenodd'
+                              d='M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5z'
+                            />
+                          </svg>
+                        </span>
+                        Subir
+                      </NavLink>
+                    </li>
 
-                  <li className='nav-item'>
-                    <NavLink
-                      to='/Favoritos'
-                      className='btn link-nav-user text-center'
-                    >
-                      <span>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          width='23'
-                          height='23'
-                          fill='currentColor'
-                          className='bi bi-suit-heart-fill'
-                          viewBox='0 0 16 16'
-                        >
-                          <path d='M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1' />
-                        </svg>
-                      </span>
-                      Mis favoritos
-                    </NavLink>
-                  </li>
-                </ul>
+                    <li className='nav-item'>
+                      <NavLink
+                        to='/Favoritos'
+                        className='btn link-nav-user text-center'
+                      >
+                        <span>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='23'
+                            height='23'
+                            fill='currentColor'
+                            className='bi bi-suit-heart-fill'
+                            viewBox='0 0 16 16'
+                          >
+                            <path d='M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1' />
+                          </svg>
+                        </span>
+                        Mis favoritos
+                      </NavLink>
+                    </li>
+                  </ul>
+                ) : (
+                  ''
+                )}
 
                 <span className='nav-item margin-nav separador'>|</span>
 
